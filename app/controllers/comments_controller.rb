3a12ba552a4bea46285a7b6bef5353d301e41b24
ForAeons <w.xy020203@gmail.com>
2023-01-06 19:10:12 +0800

    bug fixes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -75,6 +75,6 @@ class CommentsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def comment_params
-      params.require(:comment).permit(:content, :User_id, :post_id)
+      params.require(:comment).permit(:content, :user_id, :post_id)
     end
 end
--- db/migrate/20230106073727_drop_access_token.rb
+++
@@ -1,5 +0,0 @@
-class DropAccessToken < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :access_tokens
-  end
-end
