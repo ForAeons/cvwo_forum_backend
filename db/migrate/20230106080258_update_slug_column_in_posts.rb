6a348b7214c2d953d6cdc99950376230aff52fe0
ForAeons <w.xy020203@gmail.com>
2023-01-06 19:12:28 +0800

    bug fixes

--- db/migrate/20230106080258_update_slug_column_in_posts.rb
+++
@@ -1,6 +0,0 @@
-class UpdateSlugColumnInPosts < ActiveRecord::Migration[7.0]
-  def change
-    change_column_null :posts, :slug, false
-    Post.where(slug: nil).update_all(slug: 'default-slug')
-  end
-end
--- db/migrate/20230106080339_update_column_in_posts.rb
+++
@@ -1,5 +0,0 @@
-class UpdateColumnInPosts < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :posts, :slug, :string
-  end
-end
