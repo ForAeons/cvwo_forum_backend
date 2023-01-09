20102736ae6b31240c35ed2c0a69df5cde3f4795
ForAeons <w.xy020203@gmail.com>
2023-01-09 09:22:43 +0800

    dependent relationship established, deleting a parent will delete its coresponding childnre

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
+    has_many :comments, dependent: :destroy
+    has_many :posts, dependent: :destroy
     
     validates :username, presence: true, length: { in: 6..30 }, uniqueness: true
     validates :bio, length: { maximum: 300 }, allow_nil: true, presence: false
